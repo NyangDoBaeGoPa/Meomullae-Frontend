6b9848739e244e1ab08a671b8b0a1ef6a33c6240
OnlyYellow <ash1823@naver.com>
2023-01-30 20:13:30 +0900

    feat(mbti result): add frame of friendly food

--- components/MBTIresult/MBTIresult.component.tsx
+++ components/MBTIresult/MBTIresult.component.tsx
@@ -6,11 +6,14 @@ export const MBTIresult = () => {
           <p className="flex h-[40px] w-[cal(100%-90px)] mx-[45px] my-[10px] justify-center items-center text-center text-[15px] font-bold">
             MBTI 결과에 따라 나온 음식 설명입니다.
           </p>
-          <div className="flex h-[100px] w-[150px] rounded-[30px] bg-[#A8A8A8]">
+          <div className="flex h-[100px] w-[150px] rounded-[30px] bg-[#D9D9D9]">
             <img
               src="./images/blacktigerShrimp.jpg"
               className="w-[100%] object-cover rounded-[30px]"
             />
+          </div>
+          <div className="flex flex-col h-[250px] w-[calc(100%-50px)] mx-[25px] my-[10px] rounded-[30px] bg-[#D9D9D9]">
+            <p className="flex mx-auto my-[5px] text-[15px]">어느 음식이 나와 잘 맞을까요??</p>
           </div>
         </div>
       </div>
