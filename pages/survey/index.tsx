4c270b21de4e74d30b9908e9bcfc92843a223d06
DongHyunKim <akainoo123@gmail.com>
2023-02-07 16:58:58 +0900

    fix : fix the type error

--- pages/survey/index.tsx
+++ pages/survey/index.tsx
@@ -1,11 +1,11 @@
 import Box from '@mui/material/Box';
 
-import { Flow2Body, GoOrBack, HeaderLogo, LinearProgressWithLabel } from '@/components';
+import { Flow2Body, GoOrBack, LogoHeader, LinearProgressWithLabel } from '@/components';
 
 export default function Flow2() {
   return (
     <>
-      <HeaderLogo />
+      <LogoHeader />
       <LinearProgressWithLabel value={33} />
       <Box className="h-full w-full bg-[#C4E6FF] rounded-lg shadow-md">
         <Flow2Body />
