71a4ec5bc84e0e47a10940b23ed4faf54e9c0489
OnlyYellow <ash1823@naver.com>
2023-02-09 10:47:29 +0900

    feat(mbtiresult): set typography of font size

--- components/FoodRecommendation/food-recommendation.tsx
+++ components/FoodRecommendation/food-recommendation.tsx
@@ -1,14 +1,49 @@
-import { Box } from '@mui/material';
+import { Button, Typography, Box } from '@mui/material';
+import Link from 'next/link';
+import { useState } from 'react';
+import { BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill } from 'react-icons/bs';
 
-import { HeaderLogo } from '../HeaderLogo';
+import { HeaderLogo } from '@/components';
 
 export const FoodRecommendation = () => {
+  const [name] = useState(['대연각', '금룡', '홍콩반점']);
+
   return (
     <>
       <Box className="flex flex-col h-[100vh] w-full">
         <HeaderLogo />
         <Box className="flex flex-col h-full w-full p-2.5">
-          <Box className="flex flex-row justify-center items-center h-[30%] w-full px-[15%] py-2.5 bg-[#C4E6FF] rounded-[20px] shadow-md"></Box>
+          <Box className="flex flex-row justify-center items-center h-[40%] w-full px-[15%] py-2.5 bg-[#C4E6FF] rounded-[20px] shadow-md">
+            <Box className="flex justify-center items-center h-full w-[10%]">
+              <Button className="flex justify-center items-center">
+                <BsFillArrowLeftCircleFill className="flex h-full w-[50%]" />
+              </Button>
+            </Box>
+            <Box className="flex flex-col justify-evenly items-center h-full w-[80%] p-2.5">
+              <Box className="flex flex-col justify-center items-center h-[60%] w-full bg-[#FFF7CB] rounded-[20px] shadow-md">
+                <Typography variant="h5">1위</Typography>
+                <Typography variant="h6">{name[0]}</Typography>
+              </Box>
+              <Link
+                href="./"
+                className="flex flex-col justify-center items-center h-[30%] w-full"
+                style={{ textDecoration: 'none' }}
+              >
+                <Button
+                  className="flex justify-center items-center h-full w-full bg-[#FFF7CB] rounded-[20px] shadow-md"
+                  style={{ color: '#000' }}
+                >
+                  <Typography>이유가 궁금하다면?</Typography>
+                </Button>
+              </Link>
+            </Box>
+            <Box className="flex justify-center items-center h-full w-[10%]">
+              <Button className="flex justify-center items-center">
+                <BsFillArrowRightCircleFill className="flex h-full w-[50%]" />
+              </Button>
+            </Box>
+          </Box>
+          <Box className="flex flex-col justify-center items-center h-[20%] w-full bg-[#C4E6FF] rounded-[20px] shadow-md"></Box>
         </Box>
       </Box>
     </>
--- components/MBTIresult/MBTIresult.component.tsx
+++ components/MBTIresult/MBTIresult.component.tsx
@@ -12,7 +12,7 @@ export const MBTIresult = () => {
         <Box className="flex flex-col h-full w-full p-2.5">
           <Box className="flex flex-col justify-start items-center h-full w-full bg-[#C4E6FF] rounded-[20px] shadow-md">
             <Box className="flex justify-center items-center text-center w-full pt-2.5 pb-1 px-11">
-              <Typography component="h6">MBTI 결과에 따라 나온 음식 설명입니다.</Typography>
+              <Typography variant="h6">MBTI 결과에 따라 나온 음식 설명입니다.</Typography>
             </Box>
             <Box className="flex justify-center items-center pt-1.5 pb-1 px-[25%] w-full">
               <img
@@ -23,7 +23,7 @@ export const MBTIresult = () => {
             <Box className="flex flex-col items-center h-full w-full px-6 pt-1.5 pb-1">
               <Box className="flex flex-col justify-center items-center h-full w-full pb-4 bg-[#FFF7CB] rounded-[20px]">
                 <Box className="flex justify-center items-center text-center w-full py-1 px-24">
-                  <Typography component="h6">음식 궁합</Typography>
+                  <Typography>음식 궁합</Typography>
                 </Box>
                 <Box className="flex flex-row justify-between items-center h-full w-full p-4">
                   <Box className="flex h-[100px] w-[48%] rounded-[30px] bg-[#A8A8A8]">
@@ -42,7 +42,7 @@ export const MBTIresult = () => {
               </Box>
               <Box className="flex flex-col justify-center items-center pt-1.5 pb-1 px-16">
                 <Box className="flex justify-center items-center text-center w-full">
-                  <Typography component="h6">결과에 만족하시나요?</Typography>
+                  <Typography>결과에 만족하시나요?</Typography>
                 </Box>
                 <Box className="flex flex-row justify-center items-center w-full">
                   <Rating name="no-value" value={null} />
@@ -54,27 +54,27 @@ export const MBTIresult = () => {
         <Divider className="w-full" />
         <Box className="flex flex-col justify-center items-center text-center h-[40%] w-full p-2.5">
           <Box className="flex justify-center items-center text-center w-full px-21">
-            <Typography component="h6">다시 테스트 해보시겠어요?</Typography>
+            <Typography variant="h6">다시 테스트 해보시겠어요?</Typography>
           </Box>
           <Box className="flex flex-row justify-between items-center h-full w-full py-2.5">
             <Link
               href="./"
               className="flex justify-center items-center h-full w-[49%] bg-[#FFF7CB] rounded-[20px] shadow-md"
-              style={{ textDecoration: 'none' }}
+              style={{ textDecoration: 'none', color: '#000' }}
             >
               <Box className="flex flex-col justify-center items-center text-center h-full w-full">
-                <Typography component="h6">Flow1</Typography>
-                <Typography component="h6">Food details</Typography>
+                <Typography variant="h6">Flow1</Typography>
+                <Typography variant="h6">Food details</Typography>
               </Box>
             </Link>
             <Link
               href="./"
               className="flex justify-center items-center h-full w-[49%] bg-[#FFF7CB] rounded-[20px] shadow-md"
-              style={{ textDecoration: 'none' }}
+              style={{ textDecoration: 'none', color: '#000' }}
             >
               <Box className="flex flex-col justify-center items-center text-center h-full w-full">
-                <Typography component="h6">Flow2</Typography>
-                <Typography component="h6">MBTI</Typography>
+                <Typography variant="h6">Flow2</Typography>
+                <Typography variant="h6">MBTI</Typography>
               </Box>
             </Link>
           </Box>
