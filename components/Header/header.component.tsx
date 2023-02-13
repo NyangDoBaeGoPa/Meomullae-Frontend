be0ab5f8e84d0a63e164b037fa070c4fbe86fba7
OnlyYellow <ash1823@naver.com>
2023-02-13 17:44:05 +0900

    feat(header): uIProvider 지우기
    감싸고 있는 UIProvider 부분 제거

--- components/Header/header.component.tsx
+++ components/Header/header.component.tsx
@@ -1,20 +1,17 @@
 import { Box } from '@mui/material';
 
 import { Logo } from '@/components';
-import { UIProvider } from '@/provider';
 
 export const Header = () => {
   return (
     <Box component="div" className="flex w-full h-16">
-      <UIProvider>
-        <Box
-          component="div"
-          className="flex justify-center items-center w-full h-16 mx-2"
-          bgcolor="primary.main"
-        >
-          <Logo />
-        </Box>
-      </UIProvider>
+      <Box
+        component="div"
+        className="flex justify-center items-center w-full h-16 mx-2"
+        bgcolor="primary.main"
+      >
+        <Logo />
+      </Box>
     </Box>
   );
 };
