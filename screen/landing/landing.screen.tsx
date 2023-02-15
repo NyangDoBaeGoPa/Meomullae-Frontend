e59bd5cae237657cba54a1a0926c48cec3efddf2
OnlyYellow <ash1823@naver.com>
2023-02-15 21:02:01 +0900

    feat(landing screen): 각 component들의 높이 조정
    %로 높이 비율 설정

--- screen/landing/landing.screen.tsx
+++ screen/landing/landing.screen.tsx
@@ -39,19 +39,27 @@ export const LandingScreen = () => {
     <>
       <Header />
       <Modal ref={modalRef} description={ModalDescription} />
-      <Box className="flex flex-col justify-center bg-basic">
-        <Box className="pt-5 leading-9 text-center" typography="h1">
-          {IntroTitle}
-        </Box>
-        <Box className="pt-4 leading-9 text-center">
-          <Typography component="span" variant="h2">
-            {IntroDescription}
-            <Typography component="span" variant="h2_extrabold" className="pl-1 text-primary">
-              {IntroDescriptionBold}
+      <Box className="flex flex-col justify-start h-[calc(100%-60px)]">
+        <Stack
+          direction={{ xs: 'column' }}
+          spacing={{ xs: 3, md: 6 }}
+          alignItems="center"
+          justifyContent="center"
+          height={{ xs: '30%', md: '25%' }}
+        >
+          <Box className="leading-9 text-center" typography="h1">
+            {IntroTitle}
+          </Box>
+          <Box className="leading-9 text-center">
+            <Typography component="span" variant="h2">
+              {IntroDescription}
+              <Typography component="span" variant="h2_extrabold" className="pl-1 text-primary">
+                {IntroDescriptionBold}
+              </Typography>
             </Typography>
-          </Typography>
-        </Box>
-        <Box className="flex justify-center" onClick={handleClickButtonIcon}>
+          </Box>
+        </Stack>
+        <Box className="flex justify-center xs:h-[35%] md:h-[40%]" onClick={handleClickButtonIcon}>
           <Image
             src="https://res.cloudinary.com/dahw1d9li/image/upload/v1676032718/NoneBgColorChatBubble_thxlaa.png"
             alt="말풍선"
@@ -64,12 +72,14 @@ export const LandingScreen = () => {
         </Box>
         <Stack
           direction={{ xs: 'column', md: 'row' }}
-          spacing={4}
+          spacing={{ xs: 4, md: 8 }}
+          paddingY={{ xs: 4, md: 8 }}
           alignItems="center"
           justifyContent="center"
+          height={{ xs: '35%', md: '25%' }}
         >
           <Button
-            className="w-11/12 p-5 shadow-md md:w-5/12 md:h-40 rounded-main"
+            className="w-11/12 p-5 shadow-md md:w-5/12 md:max-w-xs h-[100px] md:h-40 rounded-main"
             onClick={handleClickRecommendSurveyButton}
             variant="contained"
             color="button"
@@ -77,7 +87,7 @@ export const LandingScreen = () => {
             {RecommendationSurveyButtonTitle}
           </Button>
           <Button
-            className="w-11/12 p-5 shadow-md md:w-5/12 md:h-40 rounded-main"
+            className="w-11/12 p-5 shadow-md md:w-5/12 md:max-w-xs h-[100px] md:h-40 rounded-main"
             onClick={handleClickMBTISurveyButton}
             variant="contained"
             color="button"
